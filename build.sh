1464c4e703cc5c179489624baa4d717946580c5e
Navjot <navjotsinghkanda@gmail.com>
2011-12-29 20:05:32 +0900

    updating build script

--- build.sh
+++ build.sh
@@ -1,7 +1,19 @@
+#
+# Script to build mp3 decoder application.
+# It depends upon lame library.
+#
+
+
 #set the prefix to the prefix on your system
 PREFIX=/home/navrooh/work/research/mp3decoder/lame/install/
 
 INCLUDE="-I$PREFIX/include/lame"
 LIBS="-L$PREFIX/lib -lmp3lame -DHAVE_MPGLIB"
 SOURCE="app.c utils.c"
+echo
+echo "Building MP3 Decoder..."
+echo "gcc -o app_lame $SOURCE $INCLUDE $LIBS"
 gcc -o app_lame $SOURCE $INCLUDE $LIBS
+echo
+echo "Successfully Done !!!"
+echo
