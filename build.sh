e25f9eb2cda2cbb95d168b9008ab1b3d730bf377
Navjot <navjotsinghkanda@gmail.com>
2011-12-29 16:38:06 +0900

    changed PREFIX in build script

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-INCLUDE="-I/home/navrooh/work/detect-bas/lame/install/include/lame"
-LIBS="-L/home/navrooh/work/detect-bas/lame/install/lib -lmp3lame -DHAVE_MPGLIB"
+#set the prefix to the prefix on your system
+PREFIX=/home/navrooh/work/research/mp3decoder/lame/install/
+
+INCLUDE="-I$PREFIX/include/lame"
+LIBS="-L$PREFIX/lib -lmp3lame -DHAVE_MPGLIB"
 SOURCE="app.c"
 gcc -o app_lame $SOURCE $INCLUDE $LIBS
