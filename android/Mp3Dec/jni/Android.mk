52daa6f5bc5896f731045f77219e1333a9c9c5b9
Navjot <navjotsinghkanda@gmail.com>
2012-03-04 16:47:18 +0900

    ADDING LOG MSG IN JNI WRAPPER

--- android/Mp3Dec/jni/Android.mk
+++ android/Mp3Dec/jni/Android.mk
@@ -12,5 +12,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := wblame
 LOCAL_SRC_FILES := mp3dec-jni.c bitstream.c fft.c id3tag.c mpglib_interface.c presets.c quantize.c reservoir.c tables.c util.c VbrTag.c encoder.c gain_analysis.c lame.c newmdct.c psymodel.c quantize_pvt.c set_get.c takehiro.c vbrquantize.c version.c
 
+LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 
 include $(BUILD_SHARED_LIBRARY)
--- android/Mp3Dec/jni/mp3dec-jni.c
+++ android/Mp3Dec/jni/mp3dec-jni.c
@@ -1,6 +1,10 @@
 #include <string.h>
 #include <jni.h>
+#include <android/log.h>
+
+#define MP3DEC_TAG    "[MP3DEC-JNI]"
 
 jstring Java_com_wb_mp3dec_Mp3Dec_Version( JNIEnv* env, jobject thiz ) {
+    __android_log_write(ANDROID_LOG_INFO, MP3DEC_TAG, "-- Mp3Decoder Beta 1.0 --");
     return (*env)->NewStringUTF(env, "Mp3Decoder Beta 1.0");
 }
