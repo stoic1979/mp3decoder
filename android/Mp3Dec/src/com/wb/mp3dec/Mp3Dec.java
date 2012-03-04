2b0a6060678613a8a27de76ac52fa6fbc3759d69
Navjot <navjotsinghkanda@gmail.com>
2012-03-04 16:28:46 +0900

    adding bare bones of mp3decoder for android

---
+++ android/Mp3Dec/src/com/wb/mp3dec/Mp3Dec.java
@@ -0,0 +1,15 @@
+package com.wb.mp3dec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Mp3Dec extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
