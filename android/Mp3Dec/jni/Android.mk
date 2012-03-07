1042961f9bb1d01804062471f10552bb93ec4cee
Navjot <navjotsinghkanda@gmail.com>
2012-03-07 23:21:09 +0900

    update Andorid mk and some tests

--- android/Mp3Dec/jni/Android.mk
+++ android/Mp3Dec/jni/Android.mk
@@ -10,7 +10,31 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := wblame
-LOCAL_SRC_FILES := mp3dec-jni.c bitstream.c fft.c id3tag.c mpglib_interface.c presets.c quantize.c reservoir.c tables.c util.c VbrTag.c encoder.c gain_analysis.c lame.c newmdct.c psymodel.c quantize_pvt.c set_get.c takehiro.c vbrquantize.c version.c app.c app_utils.c app_fft.c process_fft.c
+LOCAL_SRC_FILES := mp3dec-jni.c \
+    bitstream.c \
+    fft.c \
+    id3tag.c \
+    mpglib_interface.c \
+    presets.c \
+    quantize.c \
+    reservoir.c \
+    tables.c \
+    util.c \
+    VbrTag.c \
+    encoder.c \
+    gain_analysis.c \
+    lame.c \
+    newmdct.c \
+    psymodel.c \
+    quantize_pvt.c \
+    set_get.c \
+    takehiro.c \
+    vbrquantize.c \
+    version.c \
+    app.c \
+    app_utils.c \
+    app_fft.c \
+    process_fft.c
 
 LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 
--- android/Mp3Dec/src/com/wb/mp3dec/Mp3Dec.java
+++ android/Mp3Dec/src/com/wb/mp3dec/Mp3Dec.java
@@ -10,9 +10,13 @@ package com.wb.mp3dec;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.util.Log;
+import android.os.Environment;
+import java.io.File;
 
 public class Mp3Dec extends Activity
 {
+    private static final String TAG = "[Mp3DecMAIN]";
     /** Called when the activity is first created. */
     @Override
         public void onCreate(Bundle savedInstanceState)
@@ -21,7 +25,9 @@ public class Mp3Dec extends Activity
             //setContentView(R.layout.main);
             TextView  tv = new TextView(this);
             tv.setText(Version());
-            Decode("res/breaking-voices.mp3", "res/breaking-voices.wav");
+            File file = Environment.getExternalStorageDirectory();
+            Log.v(TAG, "data dir: " + file.getAbsolutePath());
+            //Decode("res/breaking-voices.mp3", "res/breaking-voices.wav");
             setContentView(tv);
         }
 
